fcada0c96c5921322d38187ac044b360e54f0796
arieljohn34 <202280027@psu.palawan.edu.ph>
2025-12-09 03:59:39 +0800

    Add brands fetching functionality using MySQL and dicttoxml

--- app.py
+++ app.py
@@ -16,6 +16,35 @@ mysql = MySQL(app)
 @app.route('/')
 def home():
     return "Welcome to the Brands REST API!"
+# Get all brands (JSON or XML)
+@app.route('/brands', methods=['GET'])
+def get_brands():
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM brands")
+    rows = cursor.fetchall()
+    cursor.close()
+
+    # Convert rows to list of dictionaries
+    brands_list = []
+    for row in rows:
+        brands_list.append({
+            'id': row[0],
+            'brand_name': row[1],
+            'model': row[2],
+            'description': row[3]
+        })
+
+    # Check for output format
+    fmt = request.args.get('format', 'json')
+    if fmt.lower() == 'xml':
+        xml_data = dicttoxml(brands_list, custom_root='brands', attr_type=False)
+        response = make_response(xml_data)
+        response.headers['Content-Type'] = 'application/xml'
+        return response
+    else:
+        return jsonify(brands_list)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
